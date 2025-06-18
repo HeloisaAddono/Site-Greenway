95d234d93c0c4442f71c2acd7b52cbc7cfc9176a
HELOÍSA PEREIRA ADDONO <54202070879@info.local>
2025-06-18 16:43:17 -0300

    Site pronto

---
+++ script.js
@@ -0,0 +1,20 @@
+ const carousel = document.querySelector('.carousel');
+  const slides = document.querySelectorAll('.slide');
+  const prevBtn = document.querySelector('.prev');
+  const nextBtn = document.querySelector('.next');
+  let currentIndex = 0;
+
+  function updateCarousel() {
+    const offset = -currentIndex * 100;
+    carousel.style.transform = `translateX(${offset}%)`;
+  }
+
+  prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    updateCarousel();
+  });
+
+  nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateCarousel();
+  });
